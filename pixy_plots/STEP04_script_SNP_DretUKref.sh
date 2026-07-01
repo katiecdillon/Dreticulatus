4c044cf876bd7d520ceb27517d6dd785b372c332
katiecdillon <47304193+katiecdillon@users.noreply.github.com>
2026-07-01 12:20:34 -0400

    Update STEP04_script_SNP_DretUKref.sh
    added DretUK to the sample list

--- pixy_plots/STEP04_script_SNP_DretUKref.sh
+++ pixy_plots/STEP04_script_SNP_DretUKref.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=iob_p
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=900gb
+#SBATCH --mem=200gb
 #SBATCH --export=NONE
 #SBATCH --time=10-00:00:00
 #SBATCH --output=%x_%j.out
@@ -44,7 +44,7 @@ done
 
 # make a list of bam files
 if [[ -f DretUKref.bamlist.txt ]]; then rm DretUKref.bamlist.txt; fi ## avoid adding to list twice
-for i in Elska Louise Penny
+for i in Elska Louise Penny DretUK
 do
     echo "$i.DretUK.sorted.bam" >> DretUKref.bamlist.txt
 done
